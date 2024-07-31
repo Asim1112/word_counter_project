9a8bfae0c10e70ad03caa8a7f452269ef2b9e5b1
Asim1112 <asimhussain787@gmail.com>
2024-07-31 05:47:06 +0500

    my first commit

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 #! /usr/bin/env node
-"use strict";
-// This line is called a shebang, it tells the OS to run it with nodejs 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
-const answers = await inquirer_1.default.prompt([
+
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
     {
         name: "Sentence",
         type: "input",
